4a95f3b91bd0d9c72da89e4207673d869a822026
foliageSea <104065741+foliageSea@users.noreply.github.com>
2025-07-04 14:52:16 +0800

    Initial commit

---
+++ lib/app/common/global.dart
@@ -0,0 +1,70 @@
+import 'package:core/core.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_template/app/controllers/controllers.dart';
+import 'package:flutter_template/app/locales/locales.dart';
+import 'package:flutter_template/db/database.dart';
+import 'package:get/get.dart';
+import 'package:get_it/get_it.dart';
+
+class Global {
+  static const String appName = "flutter_template";
+  static String appVersion = "1.0.0";
+  static final GetIt getIt = GetIt.instance;
+
+  Global._();
+
+  static void info(dynamic msg, [Object? exception, StackTrace? stackTrace]) {
+    AppLogger().info(msg, exception, stackTrace);
+  }
+
+  static List<CommonInitialize Function()> getInitializes() {
+    return [
+      () => Storage(),
+      () => Request(),
+      () => PackageInfoUtil(),
+      () => Locales(),
+    ];
+  }
+
+  static Future init() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    FlutterError.onError = (details) {
+      FlutterError.presentError(details);
+      var e = details.exception;
+      var st = details.stack;
+      AppLogger().handle(e, st);
+    };
+
+    info('应用开始初始化');
+    await initCommon();
+    initAppVersion();
+    await initDatabase();
+    registerServices();
+
+    info('应用初始化完成');
+  }
+
+  static Future initCommon() async {
+    List<CommonInitialize Function()> initializes = getInitializes();
+    for (var initialize in initializes) {
+      var instance = initialize();
+      await instance.init();
+      info(instance.getOutput());
+    }
+  }
+
+  static void registerServices() {
+    var themeController = Get.put(ThemeController());
+    themeController.init();
+  }
+
+  static Future initDatabase() async {
+    getIt.registerSingleton(AppDatabase());
+    await getIt<AppDatabase>().init(getIt);
+  }
+
+  static initAppVersion() {
+    appVersion = PackageInfoUtil().getVersion();
+  }
+}
---
+++ lib/db/database.dart
@@ -0,0 +1,36 @@
+import 'package:core/core.dart';
+import 'package:flutter_template/db/services/user_service.dart';
+import 'package:flutter_template/objectbox.g.dart';
+import 'package:get_it/get_it.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
+
+/// 文档地址
+/// https://docs.objectbox.io/
+class AppDatabase with AppLogMixin {
+  late final Store store;
+
+  late String path;
+  late GetIt getIt;
+
+  Future init(GetIt getIt) async {
+    this.getIt = getIt;
+    final dir = await getApplicationSupportDirectory();
+    path = p.join(dir.path, "db");
+    logger.log("数据库 $path");
+    store = await openStore(directory: path);
+    register();
+  }
+
+  String getPath() {
+    return path;
+  }
+
+  void register() {
+    getIt.registerSingleton<UserService>(UserServiceImpl());
+  }
+}
+
+mixin AppDatabaseMixin {
+  Store get db => GetIt.I<AppDatabase>().store;
+}
